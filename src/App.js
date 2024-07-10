ffd9e780a787e33120e8af0c1eff5b0598b620fb
Rushikesh Jagdevrao Bhumkar <94697002+Rushibhumkar@users.noreply.github.com>
2024-07-10 16:44:33 +0530

    backend is added into the file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import express from "express";
+const app = express();
 
-const App = () => {
-  return (
-    <div>
-      <h3>App.js file </h3>
-    </div>
-  );
-};
+app.get("/api/products", (req, res) => {
+  const products = [
+    { name: "rushi", age: 23, id: 1 },
+    { name: "pavan", age: 22, id: 2 },
+    { name: "manan", age: 23, id: 3 },
+    { name: "aanan", age: 24, id: 4 },
+  ];
+  console.log({ req });
+  console.log({ res });
+  console.log("reqURL", req.url, req.method, req.statusCode);
+  if (req.query.search) {
+    const filteredProducts = products.filter((item) =>
+      item.name.includes(req.query.search)
+    );
+    res.send(filteredProducts);
+    return;
+  }
+  //   setTimeout(() => {
+  //     res.send(products);
+  //   }, 3000);
+});
 
-export default App;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
